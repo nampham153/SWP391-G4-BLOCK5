cbcec209bcdb67e54fb6a73ad935d86ac199684a
MissPinkDiamond <pinkdiamond2k5@gmail.com>
2025-08-15 00:44:22 +0700

    Thêm issue

---
+++ src/main/java/com/example/swp/service/IssueService.java
@@ -0,0 +1,33 @@
+package com.example.swp.service;
+
+import com.example.swp.dto.IssueRequest;
+import com.example.swp.entity.Issue;
+import com.example.swp.enums.IssueStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IssueService {
+    List<Issue> getAllIssues();
+    Optional<Issue> getIssueById(int id);
+    Issue createIssue(IssueRequest issueRequest, String createdByType);
+    List<Issue> getIssuesByCustomerId(int customerId);
+    Issue getIssueByIdOrThrow(int id);
+    Issue saveIssue(Issue issue);
+    void deleteIssueById(int id);
+    long countAll();
+    long countByStatus(IssueStatus status);
+
+    // *** THÊM DÒNG NÀY ***
+
+
+    void updateAssignedStaffAndStatus(int id, int staffId, Boolean resolved);
+
+    void save(Issue issue);
+    Page<Issue> searchAndFilterIssues(String search, String status, Pageable pageable);
+
+    Page<Issue> getIssuesByCustomerId(int customerId, Pageable pageable);
+
+}
