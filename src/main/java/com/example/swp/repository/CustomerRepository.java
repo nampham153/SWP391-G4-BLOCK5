9f1bdf5e197aadc77843b22007c26796668f3793
Tien03 <dodoantien03@gmail.com>
2025-08-14 11:39:55 +0700

    view Customer

---
+++ src/main/java/com/example/swp/repository/CustomerRepository.java
@@ -0,0 +1,19 @@
+package com.example.swp.repository;
+
+import com.example.swp.entity.Customer;
+import com.example.swp.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
+    List<Customer> findByFullnameContainingIgnoreCase(String fullname);
+    List<Customer> findByRoleName(RoleName roleName);
+    // Trong CustomerRepository.java
+    boolean existsByEmail(String email);
+    // Nếu muốn validate phone, hãy bổ sung:
+    boolean existsByPhone(String phone);
+
+}
---
+++ src/main/java/com/example/swp/service/CustomerService.java
@@ -0,0 +1,26 @@
+package com.example.swp.service;
+
+import com.example.swp.entity.Customer;
+import com.example.swp.enums.RoleName;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerService {
+
+    List<Customer> getAll();
+    Customer getCustomer(int id);
+//    Optional<Customer> findByEmail1(String email);
+//
+//    // Thêm nếu muốn search/filter
+//    List<Customer> searchByName(String name);
+//    List<Customer> filterByRole(RoleName roleName);
+//
+//    Customer save(Customer customer);
+//    void delete(int id);
+//    Customer findByEmail(String email);
+//    // Trong CustomerService.java
+//    boolean existsByEmail(String email);
+//    boolean existsByPhone(String phone);
+
+}
