479e3c4246819f457411f579834f42c7b2c8aae3
Nguyen Dinh Trung Nguyen (K16_HL) <nguyenndthe160077@fpt.edu.vn>
2025-08-14 14:40:01 +0700

    commit 14/8

---
+++ src/main/java/com/example/swp/controller/website/StaffController.java
@@ -0,0 +1,28 @@
+package com.example.swp.controller.website;
+
+import com.example.swp.dto.StaffRequest;
+import com.example.swp.entity.Staff;
+import com.example.swp.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/staff")
+public class StaffController {
+    @Autowired
+    private StaffService staffService;
+    @GetMapping("/allstaff")
+    public List<Staff> getAllStaff() {
+        return staffService.getAllStaff();
+    }
+
+    @PostMapping("/createstaff")
+    public ResponseEntity<Staff>  createStaff(@RequestBody StaffRequest staffRequest) {
+        Staff staff = staffService.createStaff(staffRequest);
+        return ResponseEntity.ok(staff);
+    }
+
+}
