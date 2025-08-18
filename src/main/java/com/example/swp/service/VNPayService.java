3150888e2b91f03e678f4e057e8ee2103f7f88a0
Nguyen Dinh Trung Nguyen (K16_HL) <nguyenndthe160077@fpt.edu.vn>
2025-08-18 17:18:26 +0700

    commit 17/8

---
+++ src/main/java/com/example/swp/service/VNPayService.java
@@ -0,0 +1,27 @@
+package com.example.swp.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.xml.bind.DatatypeConverter;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+public interface VNPayService {
+    public String createVNPayUrl(HttpServletRequest req, long amount, int orderId) throws UnsupportedEncodingException;
+
+//    public String createVnpayUrl(String orderId, double amount, String returnUrl);
+
+    public static String hmacSHA512(String key, String data) {
+        try {
+            Mac hmac512 = Mac.getInstance("HmacSHA512");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "HmacSHA512");
+            hmac512.init(secretKeySpec);
+            byte[] bytes = hmac512.doFinal(data.getBytes(StandardCharsets.UTF_8));
+            return DatatypeConverter.printHexBinary(bytes).toUpperCase();
+        } catch (Exception ex) {
+            throw new RuntimeException("Lỗi tạo hash", ex);
+        }
+    }
+}
